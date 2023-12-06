5b18cb1ba73e3a6c4c5b91b8b7233e5f079223df
darknessgr33n1002 <145072424+darknessgr33n1002@users.noreply.github.com>
2023-12-06 15:28:41 -0800

    i gave my bots animation and soon ruby

--- RubyAdventureGailesP7/Assets/Scrips/EnemyController.cs
+++ RubyAdventureGailesP7/Assets/Scrips/EnemyController.cs
@@ -9,16 +9,19 @@ public class EnemyController : MonoBehaviour
     public bool vertical;
     public float changeTime = 3.0f;
 
-    Rigidbody rigidbody2d;
+    Rigidbody2D rigidbody2d;
 
     float timer;
     int direction = 1;
 
+    Animator animator;
+
     // Start is called before the first frame update
     void Start()
     { 
-        rigidbody2d = GetComponent<Rigidbody>();
+        rigidbody2d = GetComponent<Rigidbody2D>();
         timer = changeTime;
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -34,16 +37,28 @@ public class EnemyController : MonoBehaviour
     void FixedUpdate()
     {
         Vector2 position = rigidbody2d.position;
-        if(vertical)
+        if (vertical)
         {
+            animator.SetFloat("Move x", 0);
+            animator.SetFloat("Move Y", direction);
             position.y = position.y + Time.deltaTime * speed * direction;
         }
         else
         {
+            animator.SetFloat("Move x", direction);
+            animator.SetFloat("Move Y", 0);
             position.x = position.x + Time.deltaTime * speed * direction;
         }
         
 
         rigidbody2d.MovePosition(position);
+    }
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        RubyController player = other.gameObject.GetComponent<RubyController>();
+        if (player != null)
+        {
+            player.ChangeHealth(-1);
+        }
     }
 }
